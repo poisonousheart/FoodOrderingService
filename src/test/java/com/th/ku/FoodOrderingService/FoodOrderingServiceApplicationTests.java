c512b42330e30d5764f4671103a90241cf6b0f6f
Napat <napat.amorn@ku.th>
2020-11-12 15:00:43 +0700

    first commit

---
+++ src/test/java/com/th/ku/FoodOrderingService/FoodOrderingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th.ku.FoodOrderingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
