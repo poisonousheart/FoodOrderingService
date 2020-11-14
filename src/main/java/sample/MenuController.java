47020aba595b498da4fe964b8283c4766d00a437
Napat <napat.amorn@ku.th>
2020-11-14 17:38:16 +0700

    Create link between Menu and Order page

--- src/main/java/sample/MenuController.java
+++ src/main/java/sample/MenuController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -14,9 +15,12 @@ import java.io.IOException;
 public class MenuController {
 
     @FXML ImageView Icon;
+    @FXML Button confirmButton, backButton, orderButton;
 
     public void initialize(){
-
+        confirmButton.setVisible(false);
+        backButton.setVisible(false);
+        orderButton.setVisible(true);
     }
 
     @FXML public void exitButton(MouseEvent event) throws IOException {
@@ -28,4 +32,21 @@ public class MenuController {
         MenuController menuScene = loader.getController();
     }
 
+    @FXML public void orderButtonClick(MouseEvent event){
+        confirmButton.setVisible(true);
+        backButton.setVisible(true);
+        orderButton.setVisible(false);
+    }
+
+    @FXML public void backButtonClick(MouseEvent event){
+        confirmButton.setVisible(false);
+        backButton.setVisible(false);
+        orderButton.setVisible(true);
+    }
+
+    @FXML public void confirmButtonClick(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("OrderStatusController.fxml"));
+        Stage stage = (Stage) confirmButton.getScene().getWindow();
+        stage.getScene().setRoot(root);
+    }
 }
--- src/main/java/sample/OrderStatusController.java
+++ src/main/java/sample/OrderStatusController.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class OrderStatusController {
+    @FXML
+    Button backButton;
+
+    public void initialize(){
+
+    }
+
+    @FXML public void backButtonClick(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MenuController.fxml"));
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        stage.getScene().setRoot(root);
+    }
 }
