60ec93c6bf5ac12a483d3a0c015b9dab84ea191c
RbCabbage <48822595+RbCabbage@users.noreply.github.com>
2020-11-14 14:45:15 +0700

    Update All

--- src/main/java/sample/MenuController.java
+++ src/main/java/sample/MenuController.java
@@ -1,8 +1,13 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -16,7 +21,11 @@ public class MenuController {
 
     @FXML public void exitButton(MouseEvent event) throws IOException {
         System.out.println("Exit Program");
-
+        Label area = (Label) event.getSource();
+        Stage regisStage = (Stage) area.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("registerScene.fxml"));
+        regisStage.setScene(new Scene((Parent) loader.load(),800, 600));
+        MenuController menuScene = loader.getController();
     }
 
 }
