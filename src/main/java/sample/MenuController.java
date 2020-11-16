cd745a01c13b5d098695d8232ba524e71639a1a5
RbCabbage <48822595+RbCabbage@users.noreply.github.com>
2020-11-16 14:47:02 +0700

    Update MenuController

--- src/main/java/sample/MenuController.java
+++ src/main/java/sample/MenuController.java
@@ -3,11 +3,10 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.*;
@@ -17,32 +16,21 @@ public class MenuController {
 
     @FXML ImageView Icon;
     @FXML Button confirmButton, backButton, orderButton;
+    @FXML AnchorPane menuPage, confirmPage;
 
     public void initialize(){
-        confirmButton.setVisible(false);
-        backButton.setVisible(false);
-        orderButton.setVisible(true);
-    }
-
-    @FXML public void exitButton(MouseEvent event) throws IOException {
-        System.out.println("Exit Program");
-        Label area = (Label) event.getSource();
-        Stage regisStage = (Stage) area.getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("registerScene.fxml"));
-        regisStage.setScene(new Scene((Parent) loader.load(),800, 600));
-        MenuController menuScene = loader.getController();
+        confirmPage.setVisible(false);
+        menuPage.setDisable(false);
     }
 
     @FXML public void orderButtonClick(MouseEvent event) throws IOException, SQLException {
-        confirmButton.setVisible(true);
-        backButton.setVisible(true);
-        orderButton.setVisible(false);
+        confirmPage.setVisible(true);
+        menuPage.setDisable(true);
     }
 
     @FXML public void backButtonClick(MouseEvent event){
-        confirmButton.setVisible(false);
-        backButton.setVisible(false);
-        orderButton.setVisible(true);
+        confirmPage.setVisible(false);
+        menuPage.setDisable(false);
     }
 
     @FXML public void confirmButtonClick(MouseEvent event) throws IOException {
