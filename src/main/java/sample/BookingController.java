5adc5b353300eb149413d620c26330ca4be1ace1
RbCabbage <48822595+RbCabbage@users.noreply.github.com>
2020-11-17 18:24:08 +0700

    Edit BookingController

--- src/main/java/sample/BookingController.java
+++ src/main/java/sample/BookingController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -12,13 +12,27 @@ import java.io.IOException;
 
 public class BookingController {
 
-        @FXML ChoiceBox amount;
-        @FXML Button bookButton;
+        @FXML Label amountLabel;
+        @FXML Button increaseButton,decreaseButton,bookButton;
+
+    @FXML public void increaseButtonClick(MouseEvent event) throws IOException {
+        Integer amount = Integer.parseInt(amountLabel.getText());
+        if (amount < 10){
+            amountLabel.setText(String.valueOf(amount+1));
+        }
+    }
+
+    @FXML public void decreaseButtonClick(MouseEvent event) throws IOException {
+        Integer amount = Integer.parseInt(amountLabel.getText());
+        if (amount > 1){
+            amountLabel.setText(String.valueOf(amount-1));
+        }
+    }
 
     @FXML public void bookingButtonClick(MouseEvent event) throws IOException {
-        //IOExceptionString amountOfCustomer = (String) amount.getValue();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MenuController.fxml"));
         Stage stage = (Stage) bookButton.getScene().getWindow();
         stage.getScene().setRoot(root);
     }
-    }
+
+}
